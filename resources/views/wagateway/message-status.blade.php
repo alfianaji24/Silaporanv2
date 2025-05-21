98c38519bac0f020bb5a0780c56f673422c1ca32
ALFIAN AJI WAHYUDI <alfianajiwahyudi@gmail.com>
2025-05-21 18:48:16 +0700

    status delivery wa

--- resources/views/wagateway/message-status.blade.php
+++ resources/views/wagateway/message-status.blade.php
@@ -3,6 +3,15 @@
 @section('navigasi')
     <span>Status Pesan WhatsApp</span>
 @endsection
+
+@push('css')
+<!-- DataTables CSS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
+<!-- Toastr CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+@endpush
+
 @section('content')
     <div class="row">
         <div class="col-12">
@@ -164,6 +173,18 @@
 @endsection
 
 @push('myscript')
+<!-- jQuery (if not already included) -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<!-- DataTables JS -->
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap5.min.js"></script>
+<!-- Moment.js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+<!-- Toastr JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         let currentPage = 1;
